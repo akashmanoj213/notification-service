a2855bb0cc66e8ee499d9e52d71ec95dcc0562e7
Akash Manoj <akashmanoj213@gmail.com>
2023-01-12 12:09:00 +0530

    Refactored code

--- src/utils/clients/twillioClient.js
+++ src/utils/clients/twillioClient.js
@@ -2,11 +2,14 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const senderNumber = process.env.TWILIO_SENDER_NUMBER;
 
-const client = require('twilio')(accountSid, authToken);
+if(!authToken) {
+    console.log("Error: Please set TWILIO ENV variables...");
+}
 
 const sampleMessage = "This is a test SMS from notification service."
 
 const sendMessage = async (body = sampleMessage, receiverNumber) => {
+    const client = require('twilio')(accountSid, authToken);
     const response = await client.messages
         .create({
             body, // 'This is the ship that made the Kessel Run in fourteen parsecs?',
