c2cb4fb0374d596615c3a2b55ca4571854d6fc86
Akash Manoj <akashmanoj213@gmail.com>
2023-01-12 12:02:21 +0530

    Fixed complile time error

--- src/utils/clients/twillioClient.js
+++ src/utils/clients/twillioClient.js
@@ -2,8 +2,6 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const senderNumber = process.env.TWILIO_SENDER_NUMBER;
 
-console.log("token:", process.env.TWILIO_AUTH_TOKEN);
-
 const client = require('twilio')(accountSid, authToken);
 
 const sampleMessage = "This is a test SMS from notification service."
